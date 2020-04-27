5ed177dda7e523fc1424fa8ffddedbc91d947a40
charlie <charlie.howlett@live.co.uk>
2020-04-27 19:22:55 +0100

    initial icon-grid implementation

---
+++ src/components/FloatingIcon.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="href == null" class="floating-container" :style="'height: ' + height + 'px;'">
+    <img :src="image"/>
+  </div>
+  <div v-else class="floating-container">
+    <a :href="href">
+      <img :src="image"/>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FloatingIcon',
+    props: {
+      image: {
+        required: true,
+        type: String
+      },
+      height: {
+        required: true,
+        type: Number
+      },
+      href: String,
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  a {
+    height: 100%;
+  }
+  img {
+    transition: 0.5s;
+    box-sizing: border-box;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+  }
+  img:hover {
+    padding: 0px;
+  }
+  .floating-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.5s;
+    box-sizing: border-box;
+    // height: 200px !important;
+  }
+</style>
---
+++ src/components/IconList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="icon-list" :style="'grid-template-columns: repeat(' + rowWidth + ', 1fr);' + 'background-color: black;'">
+    <FloatingIcon 
+      v-for="item in items" 
+      :key="item.image"
+      :image="item.image" 
+      :href="item.link"
+      :style="'grid-column: span ' + item.width + ';'"
+      :height="200" />
+  </div>
+</template>
+
+<script>
+  import FloatingIcon from "./FloatingIcon.vue";
+
+  export default {
+    name: "IconList",
+    props: {
+      items: {
+        type: Array,
+        required: true
+      },
+      rowWidth: {
+        type: Number,
+        required: true
+      }
+    },
+    components: {
+      FloatingIcon,
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  #icon-list {
+    width: 100%;
+    display: grid;
+    grid-template-rows: 200px;
+  }
+</style>
+
